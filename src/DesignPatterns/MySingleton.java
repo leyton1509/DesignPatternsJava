ade29b20131b2cfab91f1ff22665055e57ac40d7
leyton1509 <taylor92@lancaster.ac.uk>
2022-12-02 09:42:48 +0000

    Added the code to the singleton class

---
+++ src/DesignPatterns/MySingleton.java
@@ -0,0 +1,32 @@
+package DesignPatterns;
+
+public class MySingleton {
+
+    // Volatile makes it more thread safe
+    private static volatile MySingleton instance;
+
+    private MySingleton(){
+        // Private constructor
+        if(instance!=null){
+            // Stops against reflection
+            throw new RuntimeException("Use get instance method to create!");
+        }
+    }
+
+    // Call get instance to get the Singleton
+    public static MySingleton getInstance(){
+        // Static thread safe implementation
+        if(instance == null){
+            synchronized (MySingleton.class){
+                if(instance == null){
+                    // Will only happen if its creating one time
+                    instance = new MySingleton();
+                }
+            }
+        }
+        return  instance;
+
+    }
+
+
+}
--- src/Driver.java
+++ src/Driver.java
@@ -1,6 +1,8 @@
+import DesignPatterns.MySingleton;
 
 public class Driver {
     public static void main(String[] args) {
+        MySingleton sing = new MySingleton();
 
     }
 }
